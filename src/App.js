ae5938651ff5cd3da6ffa4871c5846c37d509465
Justin Sarrasin <sarrasin.justin@gmail.com>
2018-12-10 23:15:25 -0500

    sass added to create app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  // constructor(){
+  //   super();
+  // }
+
+  // componentDidMount(){
+  //   axios.get('https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nfl/teams', {
+  //   })
+  //     .then(({ data }) => {
+  //       console.log('data', data);
+  //     this.setState({
+
+  //     });
+  //   })
+  // };
+  // componentDidMount(){
+  //   axios({
+  //     method:'GET',
+  //     url: 'https://api.fantasydata.net/v3/nfl/projections/json/FantasyDefenseProjectionsByGame/2016/1?',
+  //   })
+  //     .then((res) => {
+  //       console.log('data', res);
+  //     // this.setState({
+
+  //     // });
+  //   })
+  // };
+  
+
   render() {
     return (
       <div className="App">
